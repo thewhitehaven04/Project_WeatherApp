290738bda6ad8054884f317f7f674f84260290db
thewhitehaven04 <thewhitehaven04@gmail.com>
2023-03-18 15:45:42 +0100

    Initial dist subtree commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const body = document.querySelector('body');
+
+const div = document.createElement('div');
+div.textContent = 'Hello!';
+
+body?.appendChild(div);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTs7QUFFQTtBQUNBOztBQUVBLHVCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcHJvamVjdF93ZWF0aGVyYXBwLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGJvZHkgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCdib2R5Jyk7XG5cbmNvbnN0IGRpdiA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpO1xuZGl2LnRleHRDb250ZW50ID0gJ0hlbGxvISc7XG5cbmJvZHk/LmFwcGVuZENoaWxkKGRpdik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
